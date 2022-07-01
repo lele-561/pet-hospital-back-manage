919829b007836f9263dcb37b93f96129c78332cd
WangLuyi <10195101513@stu.ecnu.edu.cn>
2022-07-01 23:43:24 +0800

    v1.0

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 左侧菜单 -->
   <el-menu :collapse="isCollapse" alive-text-color="#C5A553" class="el-menu-vertical-demo"
-           default-active="1-4-1" text-color="#fff" @close="handleClose" @open="handleOpen">
+           text-color="#fff" @close="handleClose" @open="handleOpen">
     <h3>论文管理系统</h3>
     <!-- :index跳转路由使用 -->
     <el-menu-item v-for="item in noChildren" :key="item.path" :index="item.path" @click="clickMenu(item)">
@@ -14,7 +14,7 @@
         <span slot="title">{{ item.label }}</span>
       </template>
       <!-- 二级菜单 -->
-      <el-menu-item-group v-for="subItem in item.children" :key="subItem.path" :index="subItem.path">
+      <el-menu-item-group class="el-menu-vertical-demo"  v-for="subItem in item.children" :key="subItem.path" :index="subItem.path">
         <el-menu-item :index="subItem.path" @click="clickMenu(subItem)">{{ subItem.label }}</el-menu-item>
       </el-menu-item-group>
     </el-submenu>
@@ -37,24 +37,24 @@ export default {
           path: '/paperList',
           name: 'paperList',
           label: '论文列表',
-          icon: 'video-play',
+          icon: 'document',
           url: 'MallManage/MallManage'
         }, {
           path: '/datasetList',
           name: 'datasetList',
           label: '数据集列表',
-          icon: 'user',
+          icon: 's-grid',
           url: 'UserManage/UserManage'
         }, {
           path: '/systemSetting',
           label: '系统设置',
-          icon: 'location',
+          icon: 'setting',
           children: [
             {
               path: 'systemSetting/userManage',
               name: 'systemSetting_userManage',
               label: '用户管理',
-              icon: 'setting',
+              icon: 'user',
               url: 'Other/PageTwo'
             }
           ]
@@ -80,7 +80,6 @@ export default {
           });
         }
       }
-
     }
   },
   computed: {
@@ -112,5 +111,7 @@ export default {
     line-height: 48px;
   }
 }
+
+
 </style>
 
