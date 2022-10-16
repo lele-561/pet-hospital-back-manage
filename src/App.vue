1a55de6149a77714b48a8d3af5ca174c12d7b9e9
WangLuyi <10195101513@stu.ecnu.edu.cn>
2022-10-17 00:45:07 +0800

    解决同一页面不同参数访问不刷新问题

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view :key="$route.query"></router-view>
+<!--    <router-view :key="$route.path"></router-view>-->
   </div>
 </template>
 
