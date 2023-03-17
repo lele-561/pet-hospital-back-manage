6e95291078bb3ec3d72b56fd872a4e919f8623c3
WangLuyi <10195101513@stu.ecnu.edu.cn>
2023-03-17 22:51:58 +0800

    表单增加textarea选项

--- src/components/CommonForm.vue
+++ src/components/CommonForm.vue
@@ -9,6 +9,14 @@
       <el-select v-if="item.type === 'select'" v-model="formData[item.model]" clearable placeholder="请选择">
         <el-option v-for="item in item.opts" :key="item.value" :label="item.label" :value="item.value"></el-option>
       </el-select>
+      <el-input v-if="item.type === 'textarea'" type="textarea" autosize v-model="formData[item.model]"
+                :placeholder="'请输入' + item.label"></el-input>
+      <el-autocomplete v-if="item.type==='remoteSearch'"
+                       v-model="formData[item.model]"
+                       :fetch-suggestions="querySearchAsync(formData[item.model])"
+                       placeholder="请输入内容"
+                       @select="handleSelect(formData[item.model])"
+      ></el-autocomplete>
     </el-form-item>
     <el-form-item>
       <slot></slot>
@@ -17,6 +25,8 @@
 </template>
 
 <script>
+import {postFormData} from "../utils/api";
+
 export default {
   name: 'CommonForm',
   props: {
@@ -24,9 +34,48 @@ export default {
     formData: Object,       // 表单信息
     inline: Boolean
   },
+  methods: {
+    handleSelect(item) {
+      console.log(item);
+    },
+    querySearchAsync(queryString, cb) {
+      var res = []
+      if (queryString) {
+        postFormData('/test/remoteSearch', '').then((resp) => {
+          res = resp.data.result.data
+          var results = queryString ? res.filter(this.createStateFilter(queryString)) : res;
+
+          console.log(res)
+          clearTimeout(this.timeout);
+          this.timeout = setTimeout(() => {
+            cb(results);
+          }, 3000 * Math.random());
+          // (resp.data.code === 0) {
+          //   this.$message({
+          //     type: 'success',
+          //     message: resp.data.message
+          //   });
+          //   this.isShow = false;
+          //   this.search('')
+          // } else {
+          //   this.$message({
+          //     type: 'warning',
+          //     message: resp.data.message
+          //   });
+          // }
+        })
+      }
+    },
+    createStateFilter(queryString) {
+      return (state) => {
+        return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+      };
+    },
+  },
+
   data() {
     return {}
-  },
+  }
 }
 </script>
 
