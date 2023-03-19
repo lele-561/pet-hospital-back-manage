b0110892656c6fbd158953729ba847548802a6a2
WangLuyi <10195101513@stu.ecnu.edu.cn>
2023-03-19 21:12:41 +0800

    统一使用单引号

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="login-background">
-    <el-form ref="form" :model="form" :rules="rules" class="login-container" label-width="100px" status-icon>
-      <h3 class="login-title">虚拟宠物医院后台管理系统 注册</h3>
-      <el-form-item class="name" label="手机号码" label-width="80px" prop="phoneNumber">
-        <el-input v-model="form.phoneNumber" auto-complete="off" placeholder="请输入手机号码" type="input"></el-input>
+  <div class='login-background'>
+    <el-form ref='form' :model='form' :rules='rules' class='login-container' label-width='100px' status-icon>
+      <h3 class='login-title'>虚拟宠物医院后台管理系统 注册</h3>
+      <el-form-item class='name' label='手机号码' label-width='80px' prop='phoneNumber'>
+        <el-input v-model='form.phoneNumber' auto-complete='off' placeholder='请输入手机号码' type='input'></el-input>
       </el-form-item>
-      <el-form-item class="name" label="用户名" label-width="80px" prop="name">
-        <el-input v-model="form.name" auto-complete="off" placeholder="请输入用户名" type="input"></el-input>
+      <el-form-item class='name' label='用户名' label-width='80px' prop='name'>
+        <el-input v-model='form.name' auto-complete='off' placeholder='请输入用户名' type='input'></el-input>
       </el-form-item>
-      <el-form-item label="密码" label-width="80px" prop="password">
-        <el-input v-model="form.password" auto-complete="off" placeholder="请输入密码" type="password"></el-input>
+      <el-form-item label='密码' label-width='80px' prop='password'>
+        <el-input v-model='form.password' auto-complete='off' placeholder='请输入密码' type='password'></el-input>
       </el-form-item>
-      <el-form-item label="确认密码" label-width="80px" prop="passwordConfirm">
-        <el-input v-model="form.passwordConfirm" auto-complete="off" placeholder="请确认密码" type="password"></el-input>
+      <el-form-item label='确认密码' label-width='80px' prop='passwordConfirm'>
+        <el-input v-model='form.passwordConfirm' auto-complete='off' placeholder='请确认密码' type='password'></el-input>
       </el-form-item>
-      <div style="text-align: center;margin-bottom: 20px">
-        <el-button class="login-submit" type="primary" @click="register">注册</el-button>
-        <el-button class="login-submit" style="margin-left: 30px" type="primary" @click="back">返回登录</el-button>
+      <div style='text-align: center;margin-bottom: 20px'>
+        <el-button class='login-submit' type='primary' @click='register'>注册</el-button>
+        <el-button class='login-submit' style='margin-left: 30px' type='primary' @click='back'>返回登录</el-button>
       </div>
     </el-form>
   </div>
 </template>
 
 <script>
-import {postRequestJSON} from "../utils/api";
+import {postRequestJSON} from '../utils/api';
 
 export default {
   name: 'Register',
@@ -35,26 +35,26 @@ export default {
       else callback();
     };
     let valiPasswordConfirm = (rule, value, callback) => {
-      if (value === undefined || value === '') callback(new Error('密码不能为空'));
+      if (value === undefined || value === '') callback(new Error('密码不得为空'));
       else if (value !== this.form.password) callback(new Error('密码不符'));
       else callback();
     };
     return {
       form: {},
       rules: {
-        phoneNumber: [{required: true, validator: valiPhoneNumberPass, trigger: "blur"},],
+        phoneNumber: [{required: true, validator: valiPhoneNumberPass, trigger: 'blur'},],
         name: [
-          {required: true, message: "请输入用户名", trigger: "blur"},
-          {min: 3, message: "用户名长度不能小于3位", trigger: "blur"},
-          {max: 20, message: "用户名长度不能大于20位", trigger: "blur"}
+          {required: true, message: '请输入用户名', trigger: 'blur'},
+          {min: 3, message: '用户名长度不得少于3位', trigger: 'blur'},
+          {max: 20, message: '用户名长度不得多于20位', trigger: 'blur'}
         ],
         password: [
-          {required: true, message: "请输入密码", trigger: "blur"},
-          {min: 3, message: "密码长度不能小于3位", trigger: "blur"},
-          {max: 20, message: "密码长度不能大于20位", trigger: "blur"}
+          {required: true, message: '请输入密码', trigger: 'blur'},
+          {min: 3, message: '密码长度不得少于3位', trigger: 'blur'},
+          {max: 20, message: '密码长度不得多于20位', trigger: 'blur'}
         ],
         passwordConfirm: [
-          {required: true, validator: valiPasswordConfirm, trigger: "blur"},
+          {required: true, validator: valiPasswordConfirm, trigger: 'blur'},
         ]
       }
     }
@@ -69,11 +69,11 @@ export default {
             password: this.$md5(this.form.password)
           }).then((resp) => {
             if (resp.data.code === 0) {
-              this.$confirm('注册成功，是否返回登录？', "提示", {
-                confirmButtonText: "返回",
-                type: "success",
+              this.$confirm('注册成功，是否返回登录？', '提示', {
+                confirmButtonText: '返回',
+                type: 'success',
               }).then(() => {
-                this.$router.push("/login")
+                this.$router.push('/login')
               }).catch(() => {
               });
             } else if (resp.data.code === 1) {
@@ -91,7 +91,7 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang='less' scoped>
 .login-container {
   border-radius: 15px;
   background-clip: padding-box;
