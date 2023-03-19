5f9927d4baea201c992c96d756af476f3d57b52d
WangLuyi <10195101513@stu.ecnu.edu.cn>
2023-03-19 21:15:01 +0800

    统一使用单引号

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <router-view :key="$route.query"></router-view>
-<!--    <router-view :key="$route.path"></router-view>-->
+  <div id='app'>
+    <router-view :key='$route.query'></router-view>
+<!--    <router-view :key='$route.path'></router-view>-->
   </div>
 </template>
 
