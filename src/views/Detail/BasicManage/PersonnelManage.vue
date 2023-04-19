a5f7f007dc53a8b37c0ae6626e8117b76dbd051c
WangLuyi <10195101513@stu.ecnu.edu.cn>
2023-04-20 00:42:26 +0800

    fix更新或删除后页码不跳转为第一页的bug

--- src/views/Detail/BasicManage/PersonnelManage.vue
+++ src/views/Detail/BasicManage/PersonnelManage.vue
@@ -24,7 +24,10 @@
       <common-table-operator :tableData='tableData' :tableLabel='tableLabel' @changePage='search' @del='delPersonnel'
                              @edit='editPersonnel'></common-table-operator>
       <div style='text-align: center; margin-top: 10px'>
-        <el-pagination :page-count='totalPages' :page-size='pageSize' :pager-count='7' background
+        <el-pagination :page-count='totalPages'
+                       :page-size='pageSize'
+                       :current-page.sync="currentPage"
+                       :pager-count='7' background
                        layout='prev, pager, next, jumper' @current-change='handleCurrentChange'>
         </el-pagination>
       </div>
